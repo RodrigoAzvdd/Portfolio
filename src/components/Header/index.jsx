da24d59f5ea4b55abe4140f9fd07179d13a24ce3
rodrigo azevedo silva <rodrigo.azvd750@gmail.com>
2023-08-01 17:17:45 -0300

    update compents | routers

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import styles from "./style.module.css"
+
+export default function Header() {
+    return (
+        <header>
+            <Link className={styles.logoDiv} to={'/'}>
+                <img className={styles.logo} src="src/assets/Rodrigo.png" alt="" />
+            </Link>
+            <nav>
+                <Link className={styles.navLink} to={'/'}>Início</Link>
+                <Link className={styles.navLink} to={'/about'}>Sobre mim</Link>
+                <Link className={styles.navLink} to={'/contact'}>Contato</Link>
+                <Link className={styles.navLink} to={'/projects'}>Projetos</Link>
+            </nav>
+        </header>
+    )
+}
