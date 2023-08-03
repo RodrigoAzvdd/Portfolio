9e8f21b8e87f4d4ed9969839f656157beddb2ca0
rodrigo azevedo silva <rodrigo.azvd750@gmail.com>
2023-08-03 15:26:29 -0300

    update home / imgCard

--- src/pages/sections/About/index.jsx
+++ src/pages/sections/About/index.jsx
@@ -1,14 +1,25 @@
-import imgCard from '/src/imgs/profile3.jpeg';
+import imgCard from '/src/imgs/profile4.jpeg';
 import ImgCard from '../../../components/ImgCard'
 import styles from './style.module.css'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react';
 
 function About() {
+
+    useEffect(() => {
+        Aos.init({ duration: 2000 })
+    }, [])
+
+
     return (
         <section className={styles.container}>
-            <p className={styles.aboutTitle}>Sobre mim</p>
+            <p className={`${styles.aboutTitle}`}>Sobre mim</p>
             <div className={styles.aboutInfo}>
-                <ImgCard url={imgCard} alt={"cardImg"} />
-                <div className={styles.secondContentabout}>
+                <div data-aos="zoom-in" className={styles.imgContent}>
+                    <ImgCard url={imgCard} alt={"cardImg"} />
+                </div>
+                <div data-aos="zoom-in" className={styles.secondContentabout}>
                     <p className={styles.nameInfo}><span>&lt;</span> Rodrigo Azevedo <span>/&gt;</span></p>
                     <ul className={styles.InfoItems}>
                         <li className={styles.infoItem}><i className={`${styles.aboutIcon} bi bi-code-slash`}></i> Desenvolvedor Web desde 2022</li>
@@ -23,34 +34,34 @@ function About() {
                     Habilidades
                 </p>
                 <div className={styles.skillsContent}>
-                    <div className={styles.iconCard}>
+                    <div data-aos="fade-up" className={styles.iconCard}>
                         <i className={`devicon-react-original ${styles.icon} ${styles.icon1}`}></i>
                     </div>
-                    <div className={styles.iconCard}>
+                    <div data-aos="fade-down" className={styles.iconCard}>
                         <i className={`devicon-html5-plain ${styles.icon} ${styles.icon2}`}></i>
                     </div>
-                    <div className={styles.iconCard}>
+                    <div data-aos="fade-up" className={styles.iconCard}>
                         <i className={`devicon-css3-plain ${styles.icon} ${styles.icon3}`}></i>
                     </div>
-                    <div className={styles.iconCard}>
+                    <div data-aos="fade-down" className={styles.iconCard}>
                         <i className={`devicon-javascript-plain ${styles.icon} ${styles.icon4}`}></i>
                     </div>
-                    <div className={styles.iconCard}>
+                    <div data-aos="fade-up" className={styles.iconCard}>
                         <i className={`devicon-mysql-plain ${styles.icon} ${styles.icon5}`}></i>
                     </div>
-                    <div className={styles.iconCard}>
+                    <div data-aos="fade-down" className={styles.iconCard}>
                         <i className={`devicon-git-plain ${styles.icon} ${styles.icon6}`}></i>
                     </div>
-                    <div className={styles.iconCard}>
+                    <div data-aos="fade-up" className={styles.iconCard}>
                         <i className={`devicon-github-original ${styles.icon} ${styles.icon7}`}></i>
                     </div>
-                    <div className={styles.iconCard}>
+                    <div data-aos="fade-down" className={styles.iconCard}>
                         <i className={`devicon-sass-original ${styles.icon} ${styles.icon8}`}></i>
                     </div>
-                    <div className={styles.iconCard}>
+                    <div data-aos="fade-up" className={styles.iconCard}>
                         <i className={`devicon-java-plain ${styles.icon} ${styles.icon9}`}></i>
                     </div>
-                    <div className={styles.iconCard}>
+                    <div data-aos="fade-down" className={styles.iconCard}>
                         <i className={`devicon-bootstrap-plain ${styles.icon} ${styles.icon10}`}></i>
                     </div>
                 </div>
--- src/pages/sections/Contact/index.jsx
+++ src/pages/sections/Contact/index.jsx
@@ -1,21 +1,31 @@
 import styles from './style.module.css'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
 
 export default function Contact() {
+    
+    useEffect(() => {
+        Aos.init({
+            duration: 2000
+        })
+    }, [])
+    
     return (
         <>
             <section className={styles.container}>
                 <hr />
                 <p className={styles.title}>Contato</p>
                 <div className={styles.socialMedias}>
-                    <a href="https://www.linkedin.com/in/rodrigoazvd" target="_blank" className={`${styles.socialMediaLogo} ${styles.linkedin}`} rel="noreferrer">
+                    <a data-aos="zoom-out" href="https://www.linkedin.com/in/rodrigoazvd" target="_blank" className={`${styles.socialMediaLogo} ${styles.linkedin}`} rel="noreferrer">
                         <i className={`bi bi-linkedin ${styles.linkedinLogo}`}></i>
                         <p className={styles.mediaName}>LinkedIn/RodrigoAzevedo</p>
                     </a>
-                    <a className={`${styles.socialMediaLogo} ${styles.email}`}>
+                    <a data-aos="zoom-out" className={`${styles.socialMediaLogo} ${styles.email}`}>
                         <i className={`bi bi-envelope ${styles.emailLogo}`}></i>
                         <p className={styles.mediaName}>Rodrigo.azvd750@gmail.com</p>
                     </a>
-                    <a href="https://www.github.com/RodrigoAzvdd" target="_blank" className={`${styles.socialMediaLogo} ${styles.github}`} rel="noreferrer">
+                    <a data-aos="zoom-out" href="https://www.github.com/RodrigoAzvdd" target="_blank" className={`${styles.socialMediaLogo} ${styles.github}`} rel="noreferrer">
                         <i className={`bi bi-github ${styles.githubLogo}`}></i>
                         <p className={styles.mediaName}>Github/RodrigoAzvdd</p>
                     </a>
