13ef52491bb02595757fc647bdd43a9dcab77839
rodrigo azevedo silva <rodrigo.azvd750@gmail.com>
2023-08-03 16:20:20 -0300

    update

--- src/pages/sections/Contact/index.jsx
+++ src/pages/sections/Contact/index.jsx
@@ -4,13 +4,13 @@ import 'aos/dist/aos.css'
 import { useEffect } from 'react'
 
 export default function Contact() {
-    
+
     useEffect(() => {
         Aos.init({
             duration: 2000
         })
     }, [])
-    
+
     return (
         <>
             <section className={styles.container}>
@@ -21,13 +21,17 @@ export default function Contact() {
                         <i className={`bi bi-linkedin ${styles.linkedinLogo}`}></i>
                         <p className={styles.mediaName}>LinkedIn/RodrigoAzevedo</p>
                     </a>
+                    <a data-aos="zoom-out" href="https://www.github.com/RodrigoAzvdd" target="_blank" className={`${styles.socialMediaLogo} ${styles.github}`} rel="noreferrer">
+                        <i className={`bi bi-github ${styles.githubLogo}`}></i>
+                        <p className={styles.mediaName}>Github/RodrigoAzvdd</p>
+                    </a>
                     <a data-aos="zoom-out" className={`${styles.socialMediaLogo} ${styles.email}`}>
                         <i className={`bi bi-envelope ${styles.emailLogo}`}></i>
                         <p className={styles.mediaName}>Rodrigo.azvd750@gmail.com</p>
                     </a>
-                    <a data-aos="zoom-out" href="https://www.github.com/RodrigoAzvdd" target="_blank" className={`${styles.socialMediaLogo} ${styles.github}`} rel="noreferrer">
-                        <i className={`bi bi-github ${styles.githubLogo}`}></i>
-                        <p className={styles.mediaName}>Github/RodrigoAzvdd</p>
+                    <a data-aos="zoom-out" className={`${styles.socialMediaLogo} ${styles.phone}`}>
+                        <i className={`bi bi-phone ${styles.phoneLogo}`}></i>
+                        <p className={styles.mediaName}>(85) 98734-8419</p>
                     </a>
                 </div>
             </section>
