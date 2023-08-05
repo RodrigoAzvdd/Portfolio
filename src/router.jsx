8464506004ecce777e3825cab84ccee5d7afb8de
rodrigo azevedo silva <rodrigo.azvd750@gmail.com>
2023-08-04 23:41:19 -0300

    upodate

--- src/router.jsx
+++ src/router.jsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
                 element: <Home />
             },
             {
-                path: 'projects',
+                path: '/projects',
                 element: <Projects />
             }
         ]
